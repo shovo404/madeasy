6dc7e7c41c9bb25f65cb4d5f73fca69950bda952
hasanrabbi18 <rabbi15-4890@diu.edu.bd>
2025-04-15 11:03:37 +0600

    sample

--- lib/sample.dart
+++ lib/sample.dart
@@ -26,7 +26,7 @@ class SampleCollectionPageState extends State<SampleCollectionPage> {
     '10:00 AM - 11:00 AM',
     '11:00 AM - 12:00 PM',
     '2:00 PM - 3:00 PM',
-    '4:00 PM - 5:00 PM',
+    '5:00 PM - 6:00 PM',
   ];
 
   void _confirmBooking() {
